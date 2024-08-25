a779ae36aebbc4e88d18b4d4dc1cd893fbea172d
luisegarduno <luis.e.garduno@gmail.com>
2024-08-24 20:28:47 -0400

    + Initialize LinkedList class

---
+++ LinkedList.h
@@ -0,0 +1,46 @@
+//
+// Created by Luis G. on 8/24/24.
+//
+
+#ifndef SHORTESTPATH_LINKEDLIST_H
+#define SHORTESTPATH_LINKEDLIST_H
+
+#include "Node.h"
+#include <iomanip>
+#include <fstream>
+#include <climits>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::fstream;
+
+template<class T>
+class LinkedList {
+
+    Node<T> * head;
+    Node<T> * tail;
+    Node<T> * iterator;
+
+    int size;
+
+    private:
+        void remove(Node<T>*);
+
+    public:
+        LinkedList();
+
+};
+
+/**
+ * @brief Default constructor
+ *
+ * @tparam T class type
+ */
+template<class T>
+LinkedList<T>::LinkedList(): head(nullptr), tail(nullptr), iterator(nullptr), size(0){
+}
+
+
+
+#endif //SHORTESTPATH_LINKEDLIST_H
